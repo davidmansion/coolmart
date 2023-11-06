79f007b53d39c02461357a1e3cae77290a684b61
davidmansion <93778907+davidmansion@users.noreply.github.com>
2023-11-06 10:54:27 +0000

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,10 +12,7 @@ const activeChain = "binance";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider activeChain={activeChain}>
-      <ChakraProvider>
-        <Navbar/>
         <Component {...pageProps} />
-      </ChakraProvider>
     </ThirdwebProvider>
   );
 }
